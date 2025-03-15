fd1ac196400c35ee6f89a92bed02125c0978c939
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-03-15 10:17:53 -0300

    feat(NoPrimitiveData): add NoPrimitiveData class demonstrating non-primitive types

---
+++ src/main/java/TypesData/NoPrimitiveData.java
@@ -0,0 +1,20 @@
+package TypesData;
+
+public class NoPrimitiveData {
+    public static void main(String[] args) {
+        /*
+        * Type not primitive
+        *
+        * String
+        * Array
+        * Class
+        * Interface
+        * Enum
+        * Annotation
+        *
+        * */
+
+        String name = "Walber";
+        System.out.println("Name: " + name.toUpperCase());
+    }
+}
