91b602cb45893718956a0ea35c068abab62ba0b5
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-03-18 11:14:35 -0300

    feat(ArrayStructure): add ArrayStructure class demonstrating array declaration and iteration

---
+++ src/main/java/DataStructure/ArrayStructure.java
@@ -0,0 +1,26 @@
+package DataStructure;
+
+public class ArrayStructure {
+
+  public static void main(String[] args) {
+    // Declare an array
+    String [] names = new String[5];
+
+    // Assign values to the array
+    names[0] = "Foo";
+    names[1] = "Bar";
+    names[2] = "Baz";
+    names[3] = "Qux";
+    names[4] = "Quux";
+
+    // Print the array
+    for (int i = 0; i < names.length; i++) {
+      System.out.println(names[i]);
+    }
+
+    // foreach loop
+    for (String name : names) {
+      System.out.println(name);
+    }
+  }
+}
