002b449799536ba11bc940e5ef1ce07f84a905a5
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-03-20 17:40:41 -0300

    feat(Person): enhance Person class with age display and greeting methods

--- src/main/java/ObjectOrientation/Main.java
+++ src/main/java/ObjectOrientation/Main.java
@@ -10,8 +10,13 @@ public class Main {
     person.age = 30;
     person.birthday = LocalDate.of(1990, 1, 1);
 
+    String displayAge = person.displayAge();
+    int calculatedAge = person.calculateAge();
+
     System.out.println("Name: " + person.name);
-    System.out.println("Age: " + person.age);
-    System.out.println("Birthday: " + person.birthday);
+    System.out.println(displayAge);
+    System.out.println("Birthday: " + person.birthday + " (Calculated age: " + calculatedAge + ")");
+
+    person.sayHello();
   }
 }
--- src/main/java/ObjectOrientation/Person.java
+++ src/main/java/ObjectOrientation/Person.java
@@ -6,4 +6,19 @@ public class Person {
   String name;
   int age;
   LocalDate birthday;
+
+  public void sayHello() {
+    System.out.println("Hello, my name is " + name);
+  }
+
+  public String displayAge() {
+    return "I am " + age + " years old";
+  }
+
+  public int calculateAge() {
+    LocalDate currentDate = LocalDate.now();
+    int currentYear = currentDate.getYear();
+    int birthYear = birthday.getYear();
+    return currentYear - birthYear;
+  }
 }
