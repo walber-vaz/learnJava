81e838023721060c97894153755d0127f852c345
Walber Vaz da Silva <wvs.walber@gmail.com>
2025-03-23 16:38:03 -0300

    feat(Main): update main method to use Customer class and remove commented code

--- src/main/java/ObjectOrientation/Main.java
+++ src/main/java/ObjectOrientation/Main.java
@@ -5,25 +5,31 @@ import java.time.LocalDate;
 public class Main {
 
   public static void main(String[] args) {
-    Person person = new Person();
-    person.name = "John";
-    person.age = 30;
-    person.birthday = LocalDate.of(1990, 1, 1);
+//    Person person = new Person();
+//    person.name = "John";
+//    person.age = 30;
+//    person.birthday = LocalDate.of(1990, 1, 1);
+//
+//    Role admin = new Role();
+//    admin.name = "Jane";
+//    admin.age = 25;
+//    admin.birthday = LocalDate.of(1995, 1, 1);
+//    admin.nameRole = "Admin";
+//
+//    person.sayHello();
+//    System.out.println(person.displayAge());
+//    System.out.println(person.calculateAge());
+//
+//    admin.sayHello();
+//    System.out.println(admin.displayAge());
+//    System.out.println(admin.calculateAge());
+//    admin.sayRole();
+    Customer customer = new Customer();
+    customer.name = "John";
+    customer.age = 30;
+    customer.birthday = LocalDate.of(1990, 1, 1);
+    customer.nameRole = "Customer";
 
-    Role admin = new Role();
-    admin.name = "Jane";
-    admin.age = 25;
-    admin.birthday = LocalDate.of(1995, 1, 1);
-    admin.nameRole = "Admin";
-
-    person.sayHello();
-    System.out.println(person.displayAge());
-    System.out.println(person.calculateAge());
-
-    admin.sayHello();
-    System.out.println(admin.displayAge());
-    System.out.println(admin.calculateAge());
-    admin.sayRole();
-
+    customer.myRole();
   }
 }
